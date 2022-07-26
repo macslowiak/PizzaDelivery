c93ada82002d6ebd90fed89b62955a2a19500401
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-07-26 13:47:56 +0200

    Add redirect links to different endpoints

---
+++ PizzaDelivery/Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaDelivery.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
