76ed9450cee21165df907a15d4c87b2bbea6d90a
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-07-27 08:25:22 +0200

    Add custom pizza model

---
+++ PizzaDelivery/Models/PizzasModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PizzaDelivery.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTitle { get; set; }
+        public string Name { get; set; }
+        public float BasePrice { get; set; } = 2;
+        public bool TomatoSauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Peperoni { get; set; }
+        public bool Mushroom { get; set; }
+        public bool Tune { get; set; }
+        public bool Pineapple { get; set; }
+        public bool Ham { get; set; }
+        public bool Beef { get; set; }
+        public float FinalPrice { get; set; }
+    }
+}
--- PizzaDelivery/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzaDelivery/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaDelivery.Models;
 
 namespace PizzaDelivery.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
