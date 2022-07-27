7b929a96af1c5447de44606c42136bca62daf902
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-07-27 09:17:44 +0200

    Add pizza post method

--- PizzaDelivery/Models/PizzaDtoModel.cs
+++ PizzaDelivery/Models/PizzaDtoModel.cs
@@ -5,11 +5,13 @@
         public string ImageTitle { get; set; }
         public string Name { get; set; }
         public float BasePrice { get; set; } = 2;
+
+        //there should be Pizza and Ingredients models also to manage them as objects
         public bool TomatoSauce { get; set; }
         public bool Cheese { get; set; }
         public bool Peperoni { get; set; }
         public bool Mushroom { get; set; }
-        public bool Tune { get; set; }
+        public bool Tuna { get; set; }
         public bool Pineapple { get; set; }
         public bool Ham { get; set; }
         public bool Beef { get; set; }
